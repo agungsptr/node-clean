08f4d54ea99c45d4360bbb02eccaeedfe89108bd
agungsptr <agung.e.sptr@gmail.com>
2022-10-15 18:57:31 +0800

    main: remove trailing whitespcae

--- data-access/base.js
+++ data-access/base.js
@@ -2,12 +2,7 @@ const { IfEmptyThrowError } = require("../commons/checks");
 const { conditionParser } = require("../commons/utils");
 const { RepackageError } = require("../commons/errors");
 
-const baseDataAccess = ({
-  model,
-  modelName,
-  modelBuilder,
-  serialize,
-}) => {
+const baseDataAccess = ({ model, modelName, modelBuilder, serialize }) => {
   const findAll = async (queries = {}) => {
     try {
       return model.find(conditionParser(queries)).then(serialize);
@@ -15,7 +10,6 @@ const baseDataAccess = ({
       throw RepackageError(e.message);
     }
   };
-  
   const findOne = async (id) => {
     try {
       return model.findById(id).then(serialize);
@@ -23,7 +17,6 @@ const baseDataAccess = ({
       throw RepackageError(e.message);
     }
   };
-  
   const create = async (bodyData) => {
     try {
       const data = modelBuilder(bodyData);
@@ -32,12 +25,14 @@ const baseDataAccess = ({
       throw RepackageError(e.message);
     }
   };
-  
   const update = async (id, bodyData) => {
     try {
       const data = await model.findById(id).then(serialize);
-      IfEmptyThrowError(data, `Data with id: ${id} in ${modelName} is not found`);
-  
+      IfEmptyThrowError(
+        data,
+        `Data with id: ${id} in ${modelName} is not found`
+      );
+
       const dataToUpdate = modelBuilder({ ...data, ...bodyData });
       await model.findByIdAndUpdate(id, dataToUpdate);
       return { id, ...dataToUpdate };
@@ -45,19 +40,19 @@ const baseDataAccess = ({
       throw RepackageError(e.message);
     }
   };
-  
   const remove = async (id) => {
     try {
       const data = await model.findById(id).then(serialize);
-      IfEmptyThrowError(data, `Data with id: ${id} in ${modelName} is not found`);
-  
+      IfEmptyThrowError(
+        data,
+        `Data with id: ${id} in ${modelName} is not found`
+      );
       await model.findByIdAndDelete(id);
       return null;
     } catch (e) {
       throw RepackageError(e.message);
     }
   };
-  
   const removeAll = async () => {
     try {
       await model.deleteMany();
@@ -70,10 +65,10 @@ const baseDataAccess = ({
   return {
     findAll,
     findOne,
-    create, 
+    create,
     update,
     remove,
-    removeAll
+    removeAll,
   };
 };
 
