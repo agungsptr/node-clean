609177af1c4172392c7d0d769c905fd82c04a650
agungsptr <agung.e.sptr@gmail.com>
2022-10-17 01:20:08 +0800

    main: add sanitize and helmet

--- drivers/server.js
+++ drivers/server.js
@@ -4,15 +4,33 @@ const compression = require("compression");
 const logger = require("morgan");
 const routes = require("../routes");
 const config = require("../config");
+const cors = require("cors");
+const sanitize = require("sanitize");
+const helmet = require("helmet");
 
 const app = express();
 
 /** Set logger request */
 app.use(logger("dev"));
 
+/** Set cors */
+app.use(cors());
+
+/** Set helmet */
+app.use(
+  helmet({
+    dnsPrefetchControl: false,
+    frameguard: false,
+    ieNoOpen: false,
+  })
+);
+
 /** Bodyparser */
-app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+/** Sanitize */
+app.use(sanitize.middleware);
 
 /** Compression middleware */
 app.use(compression());
