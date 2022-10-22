ba14e9b228b74cab64c46ea3dc89f650b044749a
agungsptr <agung.e.sptr@gmail.com>
2022-10-22 19:20:07 +0800

    main: add auth to all routes

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,13 @@
 const { Router } = require("express");
+const middlewares = require("../middlewares");
 const students = require("./students");
 const users = require("./users");
 const auth = require("./auth");
 
 const router = Router();
 
-router.use(students);
-router.use(users);
 router.use(auth);
+router.use(middlewares.auth, users);
+router.use(middlewares.auth, students);
 
 module.exports = router;
--- routes/users/index.js
+++ routes/users/index.js
@@ -1,11 +1,10 @@
 const users = require("../../controllers/users.controller");
 const { Router } = require("express");
-const { auth } = require("../../middleware");
 
 const router = Router();
 const baseUrl = "/user";
 
-router.get(`${baseUrl}s`, auth, users.findAll);
+router.get(`${baseUrl}s`, users.findAll);
 router.get(`${baseUrl}/:id`, users.findOne);
 router.post(`${baseUrl}`, users.create);
 router.patch(`${baseUrl}/:id`, users.update);
